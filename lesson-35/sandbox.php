fcc10d6aca5efc52853467023fb49817dcd8bde9
agusrichard <agus.richard21@gmail.com>
2020-04-24 01:18:59 +0700

    Learning 35

---
+++ lesson-35/sandbox.php
@@ -0,0 +1,23 @@
+<?php 
+
+  // Superglobals
+  // Ex: $_GET, $_POST
+
+  echo $_SERVER['SERVER_NAME'] . '<br />';
+  echo $_SERVER['REQUEST_METHOD'] . '<br />';
+  echo $_SERVER['SCRIPT_FILENAME'] . '<br />';
+  echo $_SERVER['PHP_SELF'] . '<br />';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Learn PHP</title>
+</head>
+<body>
+  
+</body>
+</html>
